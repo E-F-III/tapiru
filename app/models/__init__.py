7357db6a5749cccfc03c736dcd98bf28ce028366
Benjamin Lau <benjaminkhlau@gmail.com>
2023-02-20 19:21:00 -0600

    created drinks, menu, toppings models. need to put in relationships

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,5 @@
 from .db import db
 from .user import User
 from .db import environment, SCHEMA
+from .menu import Menu
+from .toppings import Toppings
---
+++ app/models/menu.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+from .db import db
+from sqlalchemy.orm import relationship
+
+class Menus(db.Model):
+    __tablename__ = 'menus'
+    
+    id = db.column(db.integer, primary_key=True)
+    business_id = db.column(db.integer, nullable=False)
+    name = db.column(db.string(), nullable=False)
+    description = db.column(db.string(), nullable=False)
+    
+    
+    ## waiting for business table to add relationships
+    business = db.relationship("Businesses", back_populates="menu")
+    toppings_relationship = db.relationship("Toppings", back_populates="menu")
+    
+    def to_dict(self):
+        response = {
+            "id": self.id,
+            "business_id": self.business_id,
+            "name": self.name,
+            "description": self.description,
+            ## waiting for business table to add relationships stuff
+            "toppings": [toppings.to_dict() for toppings in self.toppings_relationship]
+        }
+        return response
