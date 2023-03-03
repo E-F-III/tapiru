9b3be4c7614ff3d097e092dd644a64ff69ee7bde
edgar <edgarlee135@gmail.com>
2023-03-02 17:03:24 -0800

    updated relationships

--- app/models/businessPayments.py
+++ app/models/businessPayments.py
@@ -1,12 +1,19 @@
-from .db import db
+from .db import db, environment, SCHEMA
 
 class BusinessPayment(db.Model):
   __tablename__ = "businessPayments"
+
+  if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
 
   id = db.Column("id", db.Integer, primary_key=True)
   business_id = db.Column("business_id", db.Integer, nullable=False)
   type_id = db.Column("type_id", db.Integer, nullable=False)
 
+# FOREIGN KEY
+  user_id = db.Column(db.integer, db.ForeignKey("users.id"), nullable=False)
+
+# RELATIONSHIP
   paymentTypes = db.relationship("paymentTypes", back_populates="businessPayment", cascade="all, delete")
   business = db.relationship("business", back_populates="businessPayment", cascade="all, delete")
 
