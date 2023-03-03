9f204f092791fecc28aa3eb3b872c3cd1abd68c1
Edward Felipe III <edomfel3@gmail.com>
2023-03-02 15:02:45 -1000

    Updated Drink_Prices table for better readability

--- app/models/drink_prices.py
+++ app/models/drink_prices.py
@@ -1,19 +1,26 @@
-from .db import db
+from .db import db, environment, SCHEMA
 
 class Drink_Prices(db.Model):
     __tablename__ = "drink_prices"
 
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    # columns
     id = db.column(db.integer, primary_key=True)
-    drink_id = db.column(db.String(), db.ForeignKey('drinks.id'), nullable=False)
     size = db.column(db.String(), nullable=False)
     price = db.column(db.Integer(), nullable=False)
 
+    # foreign keys
+    drink_id = db.column(db.String(), db.ForeignKey('drinks.id'), nullable=False)
+
+    # relationships
     drinks = db.relationship("Drink", back_populates="drink_types")
 
     def to_dict(self):
         return {
             "id": self.id,
-            "drink_id": self.drink_id,
             "size": self.size,
-            "price": self.price
+            "price": self.price,
+            "drink_id": self.drink_id
         }
