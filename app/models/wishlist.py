a62b5e45a13d09841b4ea019a01b1ee4637a2c98
Edward Felipe III <edomfel3@gmail.com>
2023-03-02 15:05:50 -1000

    Updated Wishlists table for better readability

--- app/models/wishlist.py
+++ app/models/wishlist.py
@@ -1,13 +1,19 @@
-from .db import db
+from .db import db, environment, SCHEMA
 
 class Wishlist(db.Model):
     __tablename__ = "Wishlists"
 
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    # columns
     id = db.Column(db.integer, primary_key=True)
 
+    # foreign keys
     owner_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     drink_id = db.Column(db.Integer, db.ForeignKey("drinks.id"), nullable=False)
 
+    # relationships
     owner = db.relationship("User", back_populates="wishlist")
     drinks = db.relationship("Drink", back_populates="wishlist")
 
