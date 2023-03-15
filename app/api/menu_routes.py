5c98457411eaf1c6a67d297b92e54c1bb3daaea6
edgar <edgarlee135@gmail.com>
2023-03-15 11:08:39 -0700

    GET menu route

--- app/api/menu_routes.py
+++ app/api/menu_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from app.models import db, Business
+from app.models import db, Menu, Business
 from ..forms.menu_form import MenuForm
 from flask_login import current_user, login_required
 from .auth_routes import validation_errors_to_error_messages
@@ -7,3 +7,7 @@ from .auth_routes import validation_errors_to_error_messages
 menu_routes = Blueprint("menus", __name__, url_prefix="/menus")
 
 # GET ALL Menus
+@menu_routes.route("")
+def all_menus():
+    menus = Menu.query.all()
+    return {"menus": [m.to_dict() for m in menus]}
