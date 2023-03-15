c2418fa14c6f00b6b101d6b10bf4b49906a535a0
edgar <edgarlee135@gmail.com>
2023-03-15 11:57:26 -0700

    DELETE menu route

--- app/api/menu_routes.py
+++ app/api/menu_routes.py
@@ -47,3 +47,15 @@ def update_menu():
         return jsonify(menu.to_dict()), 200
     else:
         return {"errors": validation_errors_to_error_messages(form.errors)}, 401
+
+#Delete menu
+@menu_routes.route("/<int:menu_id>", methods=["DELETE"])
+@login_required
+def delete_menu(menu_id):
+    menu = Menu.query.filter(Menu.id == menu_id).first()
+    db.session.delete(menu)
+    db.session.commit()
+    return (
+        jsonify({"message": "Menu successfully deleted", "status-code": 200}),
+        200
+    )
