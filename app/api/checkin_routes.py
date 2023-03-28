919c5b842a0a422aaead1e3d69e6dca9b77346ec
Edward Felipe III <edomfel3@gmail.com>
2023-03-27 20:08:45 -1000

    API Create Checkin Route created

---
+++ app/api/checkin_routes.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, jsonify, request
+from app.models import db, Checkin, Drinks, Toppings, User
+from app.forms import CheckInForm
+from flask_login import current_user, login_required
+from .auth_routes import validation_errors_to_error_messages
+
+checkin_routes = Blueprint("checkins", __name__, url_prefix="/checkins")
+
+@checkin_routes.route("/", methods=["POST"])
+@login_required
+def create_checkin():
+    '''
+    Function that creates a new checkin
+    '''
+    form = CheckInForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    drink = Drinks.query.get(form.data["item_id"])
+
+    # Check if drink exists
+    if not drink:
+        return {'errors': ['Drink not found']}, 401
+
+    if form.validate_on_submit():
+        new_checkin = Checkin(
+            user_id = current_user.id,
+            item_id = form.data["item_id"],
+            ice_level = form.data["ice_level"],
+            sugar_level = form.data["sugar_level"],
+        )
+        db.session.add(new_checkin)
+
+        # Add toppings to checkin
+        for topping in form.data["toppings"]:
+            # Check if toppings exist
+            if not Toppings.query.get(topping):
+                return {'errors': ['Topping not found']}, 401
+            # Add topping to checkin
+            new_checkin.toppings.append(Toppings.query.get(topping))
+
+        db.session.commit()
+        return jsonify(new_checkin.to_dict()), 200
+
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
--- app/forms/checkin_form.py
+++ app/forms/checkin_form.py
@@ -6,7 +6,7 @@ def validate_level(form, field):
     if not [0, 20, 40, 60, 80, 100].index(field.data):
         raise ValidationError("Levels must be in increments of 20")
 
-class CheckinForm(FlaskForm):
+class CheckInForm(FlaskForm):
     item_id = IntegerField("ItemId", validators=[DataRequired()])
     ice_level = SelectField("Ice Level", validators=[DataRequired(), validate_level])
     sugar_level = SelectField("Sugar Level", validators=[DataRequired(), validate_level])
